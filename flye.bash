392cf183fe3bcdd6077c0f493ce86a5582ee1ae1
Ben Sakdinan <143819318+bensakdinan@users.noreply.github.com>
2025-11-06 11:46:08 -0500

    Refactor flye.bash for sample processing and command update
    Updated the flye.bash script to use sample names from a specified file and modified the flye command to use high-quality reads.

--- flye.bash
+++ flye.bash
@@ -5,18 +5,15 @@
 source /mfs/bens/miniconda3/etc/profile.d/conda.sh
 conda activate flye_env
 
-parallel -j 4 '
-  
+cat /path/to/sample_names.txt | parallel -j 10 '
+  sample={}
+
   # Directories
-  reads_dir=/mfs/bens/phages_training/00_reads/demux
-  input_file="$reads_dir/11e7ac89-cfd7-4de6-917f-7fbb1b27566c_OttawaPL_barcode0{}.fastq.gz"
-  output_path=/mfs/bens/phages_training/01_assembly
+  reads_file=/path/to/00_chopper/${sample}_cleaned.fastq.gz
+  output_path=/path/to/output/01_flye/$sample
 
-  # Make output directory (-p if it does not already exist)
-  mkdir -p $output_path/{}
+  # Make output directory for each sample (-p if it does not already exist)
+  mkdir -p $output_path
 
-  # Run flye command with --meta flag for metagenome assembly
-  # && will create an out file if flye ran and exited successfully
-  flye --nano-raw $input_file --out-dir $output_path --meta -t 8 && echo 'done' > $output_path/{}_flye.done
-  
-' ::: $(seq -w 1 96) # Array of numbers between 01-96, corresponding to our 96 samples
+  flye --nano-hq $input_file --out-dir $output_path --meta -t 8 && echo 'done' > $output_path/${sample}.flye.done
+'
